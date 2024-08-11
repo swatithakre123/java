aa8b94412f40b7c32fad63c7283a5a794fd59575
swatithakre123 <swatithakre3024@gmail.com>
2024-08-11 23:11:28 +0530

    Add files via upload

---
+++ _2Fibbonacci.java
@@ -0,0 +1,17 @@
+class Fibbonacci {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=10;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i=0;i<=n;i++)
+        {
+            int c=a+b;
+            a=b;
+            b=c;
+            System.out.println(c);
+        }
+    }
+    
+}
