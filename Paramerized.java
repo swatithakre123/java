4873a74dfb9729adb49a932b2c54eef99e756920
swatithakre123 <swatithakre3024@gmail.com>
2024-08-23 15:23:36 +0530

    Add files via upload

---
+++ Paramerized.java
@@ -0,0 +1,20 @@
+ class Parameterized{
+    String name;
+    int id ;
+
+    Parameterized(String name,int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+ }
+ class Driver{
+    public static void main(String[] args) {
+        Parameterized n = new Parameterized("Swati",102);
+       
+    }
+ }
+
+   
+
+
