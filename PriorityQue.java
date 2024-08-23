10f761bcfa17a1442d3e2a692306d745589d5efb
swatithakre123 <swatithakre3024@gmail.com>
2024-08-23 15:26:55 +0530

    Add files via upload

---
+++ PriorityQue.java
@@ -0,0 +1,10 @@
+package Practise;
+
+import java.util.PriorityQueue;
+
+class PriorityQue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> q1=new PriorityQueue<>();
+        
+    }
+}
