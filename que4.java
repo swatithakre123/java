771aaf8be2a5218a6cd1010c31a054f4201caf15
swatithakre123 <swatithakre3024@gmail.com>
2024-08-27 21:38:43 +0530

    Add files via upload

---
+++ que4.java
@@ -0,0 +1,7 @@
+ class que4 {
+    public static void main(String[] args) {
+        String str1= "PHP Exercises and ";                                                                                  
+        String str =" Python Exercises";                                                                                    
+       System.out.println(str1.concat(str));
+    }
+}
