ace09a8294f68dac0d95fe601a46d0ef5b4c81b2
swatithakre123 <swatithakre3024@gmail.com>
2024-08-27 21:39:31 +0530

    Add files via upload

---
+++ que5.java
@@ -0,0 +1,24 @@
+class que5 {
+   public static void main(String[] args) {
+  String str="example.com";                                                                   
+  String str1 ="example.com";
+  String str2 ="Example.com";  
+  
+  if(str.equals(str1))
+  {
+    System.out.println("True");
+  }
+  else{
+    System.out.println("False");
+  }
+   
+   if(str.equals(str2))
+  {
+    System.out.println("True");
+  }
+  else{
+    System.out.println("False");
+  }
+   }
+
+}
