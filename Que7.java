46a149ec1939000daa48fd21be49377691b9713a
swatithakre123 <swatithakre3024@gmail.com>
2024-08-30 20:35:41 +0530

    Add files via upload

---
+++ Que7.java
@@ -0,0 +1,12 @@
+class Que7 {
+   public static void main(String[] args) {
+    String str = "Stephen Edwin King";
+    String str1 = "Walter Winchell";
+    String str2 = "Mike Royko";
+      
+    boolean s= str.equals(str1);
+      System.out.println(str+ " is equal to "+str1+" : "+s);
+      boolean s1= str.equals(str2);
+      System.out.println(str+ " is equal to "+str2+" : "+s1);
+   } 
+}
