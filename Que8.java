eb0e59d26fe9456c5dbe6edca498b97f17aa0061
swatithakre123 <swatithakre3024@gmail.com>
2024-08-30 20:37:12 +0530

    Add files via upload

---
+++ Que8.java
@@ -0,0 +1,10 @@
+ class Que8 {
+    public static void main(String[] args) {
+        String str="The quick brown fox jumps over the lazy dog";
+         char a[]=str.charAt(i) ;
+        for(int i=0;i<str.length();i++)
+        {
+            a[i]=str.charAt(i);
+        }
+    }
+}
