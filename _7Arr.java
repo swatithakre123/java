4df9715f07d1918c9a85aae87606de28d081ea96
swatithakre123 <swatithakre3024@gmail.com>
2024-09-04 10:32:44 +0530

    Add files via upload

---
+++ _7Arr.java
@@ -0,0 +1,14 @@
+package A;
+
+ class _7Arr {
+    public static void main(String[] args) {
+        
+    
+    int[] a = new int[]{2,3,7,8,4,9,5};
+    int[] b = new 
+    for(int i=0;i<a.length;i++)
+    {
+       
+    }
+}
+ }
