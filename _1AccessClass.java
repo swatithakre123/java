11b8625a549b2c76625d60e8b4c78528819185d7
swatithakre123 <swatithakre3024@gmail.com>
2024-09-04 10:22:02 +0530

    Add files via upload

---
+++ _1AccessClass.java
@@ -0,0 +1,12 @@
+package  A ;
+import 
+public class _1AccessClass {
+    public void meth()
+    {
+        System.out.println("method");
+    }
+    public static void main(String[] args) {
+        System.out.println("Public Class");
+        staticmeth.showdata2();
+    }
+}
