3bd690b67b4cb6f67dc0d1512301cd5f96ef6d8e
swatithakre123 <swatithakre3024@gmail.com>
2024-09-10 16:44:03 +0530

    Add files via upload

---
+++ CollectionEx.java
@@ -0,0 +1,17 @@
+package A;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+interface Collection1
+{
+    public void collect();
+}
+
+class CollectionEx {
+   public static void main(String[] args) {
+    Collection <Integer> Cobj = new ArrayList<>();
+    ArrayList <String> Cobj2 = new ArrayList<>();
+      List <Integer> Cobj3 = new ArrayList<>();
+   }
+}
